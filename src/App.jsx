1e6333bf0ecbf474248bcb5ed5d600dfb49998c0
Mohamed Ashraf <mohamed.ash.1523@gmail.com>
2024-05-09 22:48:08 +0300

    React project

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import logo from "../src/images/icon-dice.svg";
+import "../src/App.css";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [adviceId, setAdviceId] = useState("");
+
+  async function getAdvice() {
+    try {
+      const res = await fetch("https://api.adviceslip.com/advice");
+      const data = await res.json();
+      setAdvice(data.slip.advice);
+      setAdviceId(data.slip.id);
+    } catch (err) {
+      console.error("Error fetching advice:", err);
+    }
+  }
+
+  return (
+    <div>
+      <main className="container">
+        <AdviceNumber adviceId={adviceId} />
+        <Advice advice={advice} />
+        <DiceButton getAdvice={getAdvice} />
+      </main>
+    </div>
+  );
+}
+
+function AdviceNumber(props) {
+  return (
+    <div>
+      <p className="advice-number">
+        Advice # <span className="advice-id">{props.adviceId}</span>
+      </p>
+    </div>
+  );
+}
+
+function Advice(props) {
+  return <q className="advice">{props.advice}</q>;
+}
+
+function DiceButton(props) {
+  return (
+    <div>
+      <div onClick={props.getAdvice} className="dice-btn">
+        <div className="dice-img">
+          {/* Render the imported logo image */}
+          <img src={logo} alt="dice icon" />
+        </div>
+      </div>
+    </div>
+  );
+}
